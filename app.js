bea7e85b952d628438754c3f3b84cc4f674cecb2
citronoiseau <avroraka2004@gmail.com>
2025-07-05 19:32:25 +0000

    basic app and pool setup

---
+++ app.js
@@ -0,0 +1,43 @@
+const path = require("node:path");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const routes = require("./routes");
+const pool = require("./config/database");
+require("dotenv").config();
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      pool: pool,
+      tableName: "session",
+    }),
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+require("./config/passport");
+
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
+app.use(routes);
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
